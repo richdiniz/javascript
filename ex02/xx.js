ab7a0226bc09dcfb30d48b66ef5ac824460f94f5
Henrique Diniz <138226252+richdiniz@users.noreply.github.com>
2025-03-03 17:22:51 -0300

    jsexercise

---
+++ ex02/xx.js
@@ -0,0 +1,7 @@
+//var nome= window.prompt('Qual é seu nome?')
+//ratando numeros: 
+var n1=Number(window.prompt('digite um número:'))
+var n2=Number(window.prompt('digite outro:'))
+var nx=n1+n2
+window.alert('soma é '+nx)
+//numero->string: window.alert('união' + String(nx))
