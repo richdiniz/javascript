be75d00f0c998bbff1ef69345b9bd02e9851b2f7
Henrique Diniz <138226252+richdiniz@users.noreply.github.com>
2025-03-06 21:39:02 -0300

    ...

---
+++ ex06/a.js
@@ -0,0 +1,8 @@
+//console.log('funciona')
+var vel=30.5
+console.log('a velocidade do seu caro é '+ vel+'km/h')
+//CONDIÇÃO SIMPLES:
+if(vel>60){
+    console.log('Você ultrapassou a velocidade permitida.MULTADO!')
+}
+console.log('dirija sempre usando cinto de segurança!')
---
+++ ex06/b.js
@@ -0,0 +1,8 @@
+var pais='BR'
+console.log('SISTEMA: tu é cria ou gringo?')
+console.log('USER: vim de '+pais)
+if(pais=='BR' || 'Brasil'){
+    console.log('SISTEMA: tu é brasileiro!')
+}else{
+    console.log('SISTEMA: Alemão')
+}
