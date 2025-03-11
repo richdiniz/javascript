f2662b496c930d60c0258ae1ac3ba90af7b24910
Henrique Diniz <138226252+richdiniz@users.noreply.github.com>
2025-03-11 05:24:46 -0300

    Create a.js

---
+++ ex10/a.js
@@ -0,0 +1,13 @@
+//teste logico no inicio:
+var c=1
+while(c<=3){
+    console.log('tentativa '+c)
+    c++
+}
+
+//teste no final:
+var cb=1
+do{
+    console.log('tentativa '+cb)
+    cb++
+}while(cb<=3)
