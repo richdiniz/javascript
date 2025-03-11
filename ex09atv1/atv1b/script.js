f18554288161c05c826bdce40ac7ca5d71160973
Henrique Diniz <138226252+richdiniz@users.noreply.github.com>
2025-03-11 00:29:30 -0300

    .

--- ex09atv1/atv1b/script.js
+++ ex09atv1/atv1b/script.js
@@ -6,12 +6,12 @@ function verificar(){
     if(fano.value.length == 0 || Number(fano.value)>ano){
         window.alert('[ERRO]Verifique os dados e tente novamente!')
     }else{
-        var fsex= document.getElementById('radsex')
+        var fsex= document.getElementsByName('radsex')
         var idade= ano - Number(fano.value)
         var genero= ''
         if(fsex[0].checked){
             genero= 'Homem'
-        }else if(fsex[1].checked){
+        }else{
            genero= 'Mulher'
         }
         res.style.textAlign='center'
