02b6cd292e88ddb7a77848aa5f21901e80f78f31
Henrique Diniz <138226252+richdiniz@users.noreply.github.com>
2025-03-11 00:10:37 -0300

    .

--- ex09atv1/atv1b/script.js
+++ ex09atv1/atv1b/script.js
@@ -2,7 +2,7 @@ function verificar(){
     var data= new Date()
     var ano= data.getFullYear()
     var fano=document.getElementById('txtano')
-    var res= document.getElementById('res')
+    var res= document.querySelector('div#res')
     if(fano.value.length == 0 || Number(fano.value)>ano){
         window.alert('[ERRO]Verifique os dados e tente novamente!')
     }else{
@@ -14,6 +14,7 @@ function verificar(){
         }else if(fsex[1].checked){
            genero= 'Mulher'
         }
-        res.innerHTML= 'Detectamos '+genero+' com '+idade+'anos.'
+        res.style.textAlign='center'
+        res.innerHTML='Detectamos '+genero+' com '+idade+'anos.'
     }
 }
