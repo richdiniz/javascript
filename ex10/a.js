ddf3ad48334c19b0d277e8d1de319c100cbf9a71
Henrique Diniz <138226252+richdiniz@users.noreply.github.com>
2025-03-11 15:28:45 -0300

    Update a.js

--- ex10/a.js
+++ ex10/a.js
@@ -1,13 +1,19 @@
+//estruturas de repetição:
 //teste logico no inicio:
 var c=1
 while(c<=3){
-    console.log('tentativa '+c)
+    console.log('tentativa A '+c)
     c++
 }
 
 //teste no final:
 var cb=1
 do{
-    console.log('tentativa '+cb)
+    console.log('tentativa B '+cb)
     cb++
 }while(cb<=3)
+
+//com variavel de controle:
+for(var cx=1;cx<=3;cx++){
+    console.log('tentativa C '+cx)
+}
