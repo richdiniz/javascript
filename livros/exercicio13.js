45a8729281bf2aa516864514b2d2f8e02e5b4553
Henrique Diniz <luisdiniz2104@gmail.com>
2026-05-20 22:48:29 -0300

    .

---
+++ livros/exercicio13.js
@@ -0,0 +1,14 @@
+console.log(primeira())
+function primeira(){
+            console.log("1-antes")
+            segunda()
+            console.log("1-depois")
+        }
+        function segunda() {
+            console.log("2-antes")
+            terceira()
+            console.log("2-depois")
+        }
+        function terceira() {
+            console.log("3")
+        }
